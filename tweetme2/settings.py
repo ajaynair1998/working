32b135e20fc9fe33a90d5c236375256902b4c489
alphajam1998 <65583605+alphajam1998@users.noreply.github.com>
2020-10-01 13:48:53 +0530

    print base dir

--- tweetme2/settings.py
+++ tweetme2/settings.py
@@ -13,7 +13,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+#on same level as manage.py
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# print(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
