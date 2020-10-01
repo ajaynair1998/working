b555304d7e365b88afeb780694ed7c8e7934d4d3
alphajam1998 <65583605+alphajam1998@users.noreply.github.com>
2020-10-01 11:57:27 +0530

    changed up some urls

--- tweets/views.py
+++ tweets/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
+from .models import Tweet
 
 # Create your views here.
 
-def home_view(request,tweet_id,*args,**kwargs):
-    return HttpResponse(f'<h1>Hello world!and hello {tweet_id}</h1>')
+def home_detail_view(request,tweet_id,*args,**kwargs):
+    obj=Tweet.objects.get(id=tweet_id)
+    return HttpResponse(f'<h1>Hello world! and hello {tweet_id}-{obj.content}</h1>')
 
-def home_detail_view(request,id,*args,**kwargs):
-    return HttpResponse(f'<h1>HELLO {id}<h1>')
+def home_view(request,*args,**kwargs):
+    return HttpResponse(f'<h1>HELLO <h1>')
